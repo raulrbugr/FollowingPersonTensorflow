aa005a9948d57106c01b77faa6140fa6240ee2c2
Raúl <raulruizbueno@gmail.com>
2018-11-11 16:49:53 +0100

    Update SharedFile.sh

--- JetsonCompute/SharedFile.sh
+++ JetsonCompute/SharedFile.sh
@@ -1,5 +1,5 @@
 #Script to mount filesystem from another machine 
 #use: sshfs user@domain:/remote/directory/ /local/directory/
 
-sshfs pi@192.168.1.102://home/pi/Projects/SharedFile/ /home/nvidia/Projects/SharedFile/ 
+sshfs user@raspberryip://home/pi/Projects/SharedFile/ /home/nvidia/Projects/SharedFile/ 
 
